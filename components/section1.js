38bd3c5d318d65b420ccc2d668c059512321c831
Sarmad Rehman <sarmadr02@gmail.com>
2023-09-20 08:23:36 -0700

    2-section1-images-copied

---
+++ components/section1.js
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+export default function section1() {
+  return (
+    <section className="py-16">
+      <div className="container mx-auto md:px-20">
+        <h1 className="pb-12 text-4xl font-bold text-center">Trending</h1>
+        {Slide()}
+      </div>
+    </section>
+  );
+}
+function Slide() {
+  return (
+    <div className="grid md:grid-cols-2">
+      <div className="image">
+        <Link href={"/"}><Image src={"/images/img1.jpg"} width={"600"} height={"600"} /></Link>
+        
+      </div>
+      <div className="flex flex-col justify-center info">
+        <div className="cat">
+          <Link href={"/"} className="text-orange-600 hover:text-orange-800">Bussiness, Travel </Link>
+          <Link href={"/"} className="text-gray-800 hover:text-gray-600">-September 20, 2023</Link>
+        </div>
+        <div className="title">
+          <Link href={"/"} className="text-3xl font-bold text-gray-800 md:text-6xl hover:text-gray-600">Your most unhappy customers are your greatest source of learning</Link>
+        </div>
+        <p className="py-3 text-gray-500">
+        Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
+        </p>
+          <h1>Author </h1>
+      </div>
+
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Header from '@/components/header'
 import Head from 'next/head'
+import Format from '@/layout/format'
+import Section1 from '../components/section1'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return ( 
-    <div>
-      <Header></Header>
-    </div>
+    <Format>
+      <Section1></Section1>
+    </Format>
   )
 }
