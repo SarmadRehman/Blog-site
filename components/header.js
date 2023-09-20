46d510c07fdefc6599afb8619775ba94fc4360bf
Sarmad Rehman <sarmadr02@gmail.com>
2023-09-19 17:15:37 -0700

    1-bare-header-without-icons

--- components/header.js
+++ components/header.js
@@ -3,15 +3,15 @@
 export default function header() {
   return (
     <header className="bg-gray-50">
-      <div className="xl:container xl:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center py-3">
-        <div>
-          <input type="text" placeholder="Search" />
+      <div className="flex flex-col items-center py-3 text-center xl:container xl:mx-auto sm:flex-row sm:justify-between">
+        <div className="flex justify-center order-2 py-4 md:flex-none w-96 sm:order-1 sm:py-0">
+          <input type="text" className="input-text" placeholder="Search" />
         </div>
-        <div>
+        <div className="shrink w-80 sm:order-2">
           <a>Design</a>
         </div>
-        <div>
-          <div className="flex">
+        <div className="flex justify-center order-3 w-96">
+          <div className="flex gap-6">
             <a>Facebook</a>
             <a>Youtube</a>
             <a>Facebook</a>
